d6e080ca98d100def2ee919662017a791f813642
jefferyfry <jeff@jefferyfry.com>
2020-04-22 16:09:32 -0700

    Updated domain.

--- api/path.go
+++ api/path.go
@@ -8,8 +8,8 @@ import (
 func GetApiService() (http.Handler) {
 	handler := GetMathHandler()
 
-	apiService := mux.NewRouter()
-	apiService.HandleFunc("/",handler.welcome)
+	apiService := mux.NewRouter().StrictSlash(true)
+	apiService.Path("/").HandlerFunc(handler.welcome)
 	apiService.Methods(http.MethodGet).Path("/add").HandlerFunc(handler.addition)
 	apiService.Methods(http.MethodGet).Path("/subtract").HandlerFunc(handler.subtraction)
 	apiService.Methods(http.MethodGet).Path("/multiply").HandlerFunc(handler.multiplication)
