9419cb74ddae6dc99d259462deb7d63c0f4aa871
jefferyfry <jeff@jefferyfry.com>
2020-04-22 20:01:08 -0700

    Test.

--- api/path.go
+++ api/path.go
@@ -10,10 +10,10 @@ func GetApiService() (http.Handler) {
 
 	apiService := mux.NewRouter();
 	apiService.Methods(http.MethodGet).Path("/status").HandlerFunc(handler.status)
-	apiService.Methods(http.MethodGet).Path("/add").HandlerFunc(handler.addition)
+/*	apiService.Methods(http.MethodGet).Path("/add").HandlerFunc(handler.addition)
 	apiService.Methods(http.MethodGet).Path("/subtract").HandlerFunc(handler.subtraction)
 	apiService.Methods(http.MethodGet).Path("/multiply").HandlerFunc(handler.multiplication)
-	apiService.Methods(http.MethodGet).Path("/divide").HandlerFunc(handler.division)
+	apiService.Methods(http.MethodGet).Path("/divide").HandlerFunc(handler.division)*/
 
 	apiService.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/status", http.StatusFound)
